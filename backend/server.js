fc5a3e91af7dbf002c18dd5828048f82d68f0f13
Douglas Rosenberg <doug.rosenberg@gmail.com>
2021-03-13 22:30:01 -0600

    started video 2

---
+++ backend/server.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const todoRoutes = express.Router();
+const PORT = 4000;
+
+let Todo = require('./todo-model');
+
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb+srv://doug:blah@cluster0.hxymn.mongodb.net/mern-todo-app?retryWrites=true&w=majority',
+    { useNewUrlParser: true },
+    { useUnifiedTopology: true }
+);
+const connection = mongoose.connection;
+
+connection.once('open', function() {
+    console.log('MongoDB DB connection established successfully')
+})
+
+todoRoutes.route('/').get(function(req, res) {
+    Todo.find(function(err, todos) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(todos);
+        }
+    });
+});
+
+todoRoutes.route('/:id').get(function(req, res) {
+    let id = req.params.id;
+    Todo.findById(id, function(err, todo) {
+        res.json(todo);
+    });
+});
+
+todoRoutes.route('/add').post(function(req, res) {
+    let todo = new Todo(req.body);
+    todo.save()
+        .then(todo => {
+            res.status(200).json({'todo': 'todo added successfully'});
+        })
+        .catch(err => {
+            res.status(400).send('adding new todo failed');
+        });
+});
+
+todoRoutes.route('/update')
+
+app.use('/todos', todoRoutes);
+
+app.listen(PORT, function() {
+    console.log('Server is running on Port: ' + PORT);
+});
